42dbe2346a8583c4432c273904b28433d5f16a2c
BhhavyaS <121062244+BhhavyaS@users.noreply.github.com>
2025-04-26 16:18:46 -0700

    Update MenstrualEquity.java added location stuff

--- MenstrualEquity/src/SJHacks/MenstrualEquity.java
+++ MenstrualEquity/src/SJHacks/MenstrualEquity.java
@@ -2,8 +2,26 @@ package SJHacks;
 
 import MVCFramework.Model;
 
+import java.util.*;
+
 public class MenstrualEquity extends Model {
+
+    private String language;
+    protected HashMap<Sector,Location> locations;
+
     public MenstrualEquity() {
         super();
     }
+    public void addLocation(Location loc){
+        locations.put(loc.getSector(),loc);
+        changed();
+    }
+    public Location getLocation(Sector sector){
+        return locations.get(sector);
+    }
+    
+
+
+
+    //sets language here
 }
