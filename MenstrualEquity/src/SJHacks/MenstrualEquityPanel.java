d6c91bee64759750d98ab8cbb028f212a351a056
japjikbatra <japjikbatra@gmail.com>
2025-04-26 16:19:11 -0700

    button stuff

--- MenstrualEquity/src/SJHacks/MenstrualEquityPanel.java
+++ MenstrualEquity/src/SJHacks/MenstrualEquityPanel.java
@@ -1,16 +1,17 @@
 package SJHacks;
 
 import Customizations.MineFieldFactory;
-import MVCFramework.AppFactory;
-import MVCFramework.AppPanel;
-import MVCFramework.Utilities;
+import MVCFramework.*;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 public class MenstrualEquityPanel extends AppPanel {
+    MenstrualEquity menstrualEquity;
     JPanel langPanel;
     JPanel sectorPanel;
     JPanel zonePanel;
+    JComboBox<String> dropdown;
     public MenstrualEquityPanel(AppFactory factory) {
         super(factory);
         this.factory = factory;
@@ -25,7 +26,7 @@ public class MenstrualEquityPanel extends AppPanel {
     private String selectLanguage(){
         langPanel = new JPanel();
         String[] langOptions = {"English", "Spanish", "Punjabi"};
-        JComboBox<String> dropdown = new JComboBox<>(langOptions);
+        dropdown = new JComboBox<>(langOptions);
         langPanel.add(dropdown);
         controlPanel.add(langPanel);
         Object selected = dropdown.getSelectedItem();
@@ -36,18 +37,39 @@ public class MenstrualEquityPanel extends AppPanel {
     private String selectLocation(){
         zonePanel = new JPanel();
         String[] zoneOptions = {"ALVISO", "BERRYESSA", "RINCONGOLDENTRIANGLE", "RINCONSOUTH", "AIRPORT", "ALUMROCK", "EVERGREEN", "SILVERCREEKVALLEY", "LITTLEPORTUGAL", "OLINDERBONITA", "LITTLESAIGON", "STORYANDKING", "YERBABUENA", "MCLAUGHLIN", "NORTHMONTEREY", "SEVENTREEW", "EDENVALE", "BASKINGRIDGE", "SANTATERESA", "BLOSSOMVALLEY", "ALMADEN", "CANOAS", "COMMUNICATIONSHILL", "WILLOWGLEN", "CAMBRIAN", "BASCOM", "FRUITDALE", "WINCHESTER", "PASEODESARATOGA", "CALABAZAS", "STEVENSCREEK", "SANTANAROWVALLEYFAIR", "DOWNTOWN", "ALMA", "COLEMAN", "CIVICCENTER", "HYDEPARK", "JAPANTOWN", "MIDTOWN", "NAGLEEPARK", "NORTHSIDE", "SPARTANKEYES", "SANTOMASAQUINO", "TAMIEN", "THEALAMEDA", "WASHINGTONGUADALUPE", "STLEOS", "ROSEGARDEN", "SHASTAHANCHETTPARK"};
-        JComboBox<String> dropdown = new JComboBox<>(zoneOptions);
+        dropdown = new JComboBox<>(zoneOptions);
         zonePanel.add(dropdown);
+        dropdown.addActionListener(this);
         controlPanel.add(zonePanel);
         Object selected = dropdown.getSelectedItem();
         String zone = (String) selected;
         return zone;
     }
 
-    //private voi
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        try {
 
-    private void createControlPanel() {
+            String selectedValue = dropdown.getSelectedItem().toString();
+            System.out.println(selectedValue);
+            menstrualEquity.addLocation();
 
+
+            String cmmd = ae.getActionCommand();
+
+
+            if (cmmd.equals("About")) {
+                Utilities.inform(factory.about());
+            } else if (cmmd.equals("Help")) {
+                Utilities.inform(factory.getHelp());
+            } else {
+                Command command = factory.makeEditCommands(model, cmmd, ae.getSource());
+                command.execute();
+                repaint();
+            }
+        } catch (Exception e) {
+            handleException(e);
+        }
     }
 
     public static void main(String[] args){
